9f0c2f51fcb68282a27acf45217fe85abeebbb31
mdsung <mdskylover@gmail.com>
2021-07-20 13:53:49 +0900

    chapter 7 convolutional layer

--- book1/chapter7_im2col.py
+++ book1/chapter7_im2col.py
@@ -18,15 +18,17 @@ def im2col(input_data, filter_h, filter_w, stride = 1, pad = 0):
             col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
     col = col.transpose[0, 4, 5, 1, 2 ,3].reshape[N * out_h * out_w, -1]
 
-#%%
-input_data = np.random.rand(10, 3, 7, 7)
-filter_h, filter_w = 3, 3
-stride = 1
-pad = 0
-N, C, H, W = input_data.shape
-print(f'N, C, H, W = {N}, {C}, {H}, {W}')
-out_h = (H + 2*pad - filter_h) // stride + 1
-out_w = (W + 2*pad - filter_w) // stride + 1
-print(f'out_h = {out_h}; out_w = {out_w}')
-img = np.pad(input_data, [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
-img
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    N, C, H, W = input_shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
